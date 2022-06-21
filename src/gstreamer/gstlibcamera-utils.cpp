b76f100bc8ed22330a43059e3848070677646469
Rishikesh Donadkar <rishikeshdonadkar@gmail.com>
2022-06-21 21:10:01 +0530

    gstreamer: Update the obtained colorimetry in caps.
    If the colorspace is set in the StreamConfiguration, convert the
    libcamera colorspace to GStreamer colorimetry.
    
    If the colorimetry corresponding to the colorspace set in the
    StreamConfiguration is not available in GStreamer set the colorimetry
    field to nullptr in the caps (this will fail the negotiation).
    
    Signed-off-by: Rishikesh Donadkar <rishikeshdonadkar@gmail.com>

--- src/gstreamer/gstlibcamera-utils.cpp
+++ src/gstreamer/gstlibcamera-utils.cpp
@@ -166,11 +166,26 @@ gst_libcamera_stream_configuration_to_caps(const StreamConfiguration &stream_cfg
 {
 	GstCaps *caps = gst_caps_new_empty();
 	GstStructure *s = bare_structure_from_format(stream_cfg.pixelFormat);
+	gchar *colorimetry = nullptr;
+	std::optional<ColorSpace> colorspace = stream_cfg.colorSpace;
 
 	gst_structure_set(s,
 			  "width", G_TYPE_INT, stream_cfg.size.width,
 			  "height", G_TYPE_INT, stream_cfg.size.height,
 			  nullptr);
+
+	if (colorspace) {
+		colorimetry = colorimerty_from_colorspace(colorspace);
+		if (colorimetry) {
+			gst_structure_set(s, "colorimetry", G_TYPE_STRING, colorimetry, nullptr);
+		} else {
+			gst_structure_set(s, "colorimetry", G_TYPE_STRING, nullptr, nullptr);
+			g_free(colorimetry);
+		}
+	} else {
+		g_free(colorimetry);
+	}
+
 	gst_caps_append_structure(caps, s);
 
 	return caps;
