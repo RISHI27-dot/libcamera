3d7f78d3eba83ddf799a0dd7f4ee4c1f560eb3c9
Rishikesh Donadkar <rishikeshdonadkar@gmail.com>
2022-08-21 17:18:09 +0530

    gstreamer: Provide framerate support to libcamerasrc.
    This patch aims to add framerate support to libcamerasrc.
    
    Add a field of the type ControlList to GstLibcameraSrcState.
    Get the framerate from the caps and convert it to FrameDuration.
    Set the FrameDurationLimits control in the initControls_ and pass
    the initControls_ at the time of starting camera.
    
    Signed-off-by: Rishikesh Donadkar <rishikeshdonadkar@gmail.com>

--- src/gstreamer/gstlibcamera-utils.cpp
+++ src/gstreamer/gstlibcamera-utils.cpp
@@ -240,13 +240,18 @@ gst_libcamera_configure_stream_from_caps(StreamConfiguration &stream_cfg,
 void
 gst_libcamera_configure_controls_from_caps(ControlList &controls, [[maybe_unused]] GstCaps *caps)
 {
-	// read framerate from caps - convert to integer and set to frame_time.
+	/* read framerate from caps - convert to integer and set to frame_time. */
+	GstStructure *s = gst_caps_get_structure(caps, 0);
+	gint fps_n = -1, fps_d = -1;
+	if (gst_structure_has_field(s, "framerate"))
+		gst_structure_get_fraction(s, "framerate", &fps_n, &fps_d);
 
-	guint frame_time = 100;
+	if (fps_n < 0 || fps_d < 0)
+		return;
+
+	gdouble frame_duration = static_cast<double>(fps_d) / static_cast<double>(fps_n) * 1000000.0;
 	controls.set(controls::FrameDurationLimits,
-		     Span<const int64_t, 2>({ frame_time, frame_time }));
-
-	// DEBUG:: use ControlList::get() api to see frameDurationLimits is really set in controls
+		     Span<const int64_t, 2>({ static_cast<int64_t>(frame_duration), static_cast<int64_t>(frame_duration) }));
 }
 
 #if !GST_CHECK_VERSION(1, 17, 1)
